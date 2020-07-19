319ecc57b8f4fb6400824dbb06983ff62c932ee4
Mikołaj Walanus <mikolaj@walanus.com>
2020-07-19 15:33:51 +0000

    ver 1.0 ES6

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-class God{
-/**
- * @returns Human[]
- */
-  static create(){
-    // code
+class God {
+  static create() {
+    return [new Man, new Woman];
   }
 }
-// code
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}
+
+// adam = new Man;
+// eve = new Woman;
+
+console.log(God.create());
+
+// console.log(God.humans)
+
+
+// console.log(Go)
+
+// The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
